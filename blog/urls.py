674dff8211b1a6b97f1df735130cac304192bcd0
Daphne <moguella.daphne@gmail.com>
2017-11-04 14:16:21 +0100

    Weer wat toegevoegd

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^A', views.a, name='a_view'),
+    url(r'^B', views.b, name='b_view'),
+    url(r'^C', views.c, name='c_view'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,14 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
     
+
+def a(request):
+	return render(request, 'blog/A.html', {})
+
+def b(request):
+	return render(request, 'blog/B.html', {})
+
+def c(request):
+	return render(request, 'blog/C.html', {})
+
+	
