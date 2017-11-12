99c1ec4409afa07d586d6130a3993357ecebf08c
Daphne <moguella.daphne@gmail.com>
2017-11-12 14:32:28 +0100

    Weer iets toe gevoegd

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,13 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+	return render(request, 'blog/post_list.html', {})
     
 
 def a(request):
-	return render(request, 'blog/A.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/A.html', {'posts': posts})
+
 
 def b(request):
 	return render(request, 'blog/B.html', {})
